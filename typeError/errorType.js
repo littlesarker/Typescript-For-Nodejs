00edcb5a5b106b6d809c5eea6a7966ad84a0a178
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-06-28 14:36:16 +0600

    Type error if type does not match

---
+++ typeError/errorType.js
@@ -0,0 +1,12 @@
+/*
+ typescript  will give error if type not match
+(Both type will give error if type does not match)
+*/
+// Expilicit
+var ex = "Explicit Variable";
+ex = 33;
+//impilicit 
+var im = "implicit variable";
+im = false;
+console.log(ex);
+console.log(im);
---
+++ typeError/errorType.ts
@@ -0,0 +1,17 @@
+/*
+ typescript  will give error if type not match
+(Both type will give error if type does not match)
+*/
+
+// Expilicit
+let ex:string ="Explicit Variable";
+ex=33;
+
+
+//impilicit 
+let im="implicit variable";
+im=false;
+
+console.log(ex);
+
+console.log(im);
