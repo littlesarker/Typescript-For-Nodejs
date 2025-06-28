6bc0f4e75bf95a66fd5c702f712dab41de833d01
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-06-28 14:17:31 +0600

    Add files via upload

---
+++ helloworld/hello.ts
@@ -0,0 +1,7 @@
+let message :string ="hello Rahim WElocome typescript";
+
+console.log(message);
+
+
+// for initialize after creating folder type "tsc filename.ts"
+// then for ts config file type npx tsc --init 
