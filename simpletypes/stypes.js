5b4f1730355118d470ed32da0e76ebb1a2cae2b3
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-06-28 14:18:14 +0600

    simple types

---
+++ simpletypes/stypes.js
@@ -0,0 +1,17 @@
+/*
+   There is 2 ways in TypeScript assigns a type
+
+   1.Explicit
+     Explicit - writing the type
+
+   2.Implicit
+     Implicit - TypeScript will "guess" the type, based on the assigned value
+
+
+*/
+//Explicit 
+var explicit = "Explicit implicit";
+console.log(explicit);
+//implicit
+var nam = "implicit  littlesarker";
+console.log(typeof nam);
---
+++ simpletypes/stypes.ts
@@ -0,0 +1,23 @@
+
+/* 
+   There is 2 ways in TypeScript assigns a type
+
+   1.Explicit
+     Explicit - writing the type
+
+   2.Implicit
+     Implicit - TypeScript will "guess" the type, based on the assigned value
+
+
+*/
+
+
+//Explicit 
+let explicit:string ="Explicit implicit";
+console.log(explicit);
+
+//implicit
+let nam="implicit  littlesarker";
+console.log(typeof nam);
+
+
