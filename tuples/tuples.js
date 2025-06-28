bd2b7a98cc2b1a7304fc6f60bfd07994a73c1b5e
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-06-28 16:06:00 +0600

    If you want an array of fixed length and types, use a tuple
     If you want an array of fixed length and types, use a tuple

---
+++ tuples/tuples.js
@@ -0,0 +1,5 @@
+/*
+  If you want an array of fixed length and types, use a tuple
+*/
+var tupe = ["littlesarer", 2025];
+console.log(tupe);
---
+++ tuples/tuples.ts
@@ -0,0 +1,7 @@
+/*
+  If you want an array of fixed length and types, use a tuple
+*/
+
+let tupe:[String,number]=["littlesarer",2025];
+
+console.log(tupe);
