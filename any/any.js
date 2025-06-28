17bffdb453bfd341a14ff73287a069070aa6d8f8
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-06-28 15:21:04 +0600

    Allows all type

---
+++ any/any.js
@@ -0,0 +1,8 @@
+/*
+
+The type (any) disable type checking and allows all Type
+
+*/
+var v = "I am littlesarker";
+v = true;
+console.log(v);
---
+++ any/any.ts
@@ -0,0 +1,11 @@
+/*
+
+The type (any) disable type checking and allows all Type
+
+*/
+
+
+let v : any ="I am littlesarker";
+v=true;
+
+console.log(v);
