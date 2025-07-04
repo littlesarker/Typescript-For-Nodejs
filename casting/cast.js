07f7e8c0f32a3a63f5ae14d9b8a58e4bcb668ee6
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-07-05 05:50:02 +0600

    Casting is the process of overriding a type.

---
+++ casting/cast.js
@@ -0,0 +1,6 @@
+// Casting with  " as "
+var x = "hello";
+console.log(x.length);
+//casting with < >
+var xy = 'rahim Sarker';
+console.log(x.length);
---
+++ casting/cast.ts
@@ -0,0 +1,14 @@
+
+// Casting with  " as "
+
+let x: unknown ="hello";
+
+console.log( ( x as string).length);
+
+
+//casting with < >
+
+let xy: unknown='rahim Sarker';
+
+console.log((<string>x).length);
+
